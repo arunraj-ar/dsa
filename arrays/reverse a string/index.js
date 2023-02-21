8a9b3aa39ea9320b5a34abb2a84fd5242e82caf8
arunraj.ar <arunraj.ar@tcs.com>
2023-02-22 01:10:02 +0530

    added single line function for reverse a string

--- arrays/reverse a string/index.js
+++ arrays/reverse a string/index.js
@@ -12,6 +12,8 @@ const reverse = (str) => {
   return backwards.join("");
 };
 
+const reverseBuiltIn = (str) => [...str].reverse().join("");
+
 const reverseWords = (str) => {
   if (!str || str.length < 2 || typeof str !== "string") {
     return str;
@@ -46,8 +48,10 @@ const findNextSpace = (str) => {
   return str.length;
 };
 
-const input = "Proident irure ea cupidatat aute ea aute ea do nisi laborum officia eu non velit. Anim cillum nulla mollit et ad occaecat excepteur laboris mollit esse anim cillum. Cillum excepteur mollit id tempor incididunt adipisicing veniam nostrud consequat est anim in. Do exercitation excepteur qui nostrud irure dolor in proident ullamco. Nisi veniam nisi velit proident enim labore voluptate officia mollit consequat do adipisicing ea fugiat. Et do deserunt proident exercitation ea officia occaecat reprehenderit id sunt deserunt cupidatat.";
+const input =
+  "Proident irure ea cupidatat aute ea aute ea do nisi laborum officia eu non velit. Anim cillum nulla mollit et ad occaecat excepteur laboris mollit esse anim cillum. Cillum excepteur mollit id tempor incididunt adipisicing veniam nostrud consequat est anim in. Do exercitation excepteur qui nostrud irure dolor in proident ullamco. Nisi veniam nisi velit proident enim labore voluptate officia mollit consequat do adipisicing ea fugiat. Et do deserunt proident exercitation ea officia occaecat reprehenderit id sunt deserunt cupidatat.";
 const output = reverseWords(input);
 document.getElementById("output").innerHTML = `<h2>${output}</h2>`;
 console.log(input);
 console.log(output);
+console.log(reverseBuiltIn(input));
