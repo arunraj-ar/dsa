11c2a5816e60491fb544c6ed16102b8510c5a1fd
arunraj-ar <mail.arunrajar@gmail.com>
2023-08-17 23:54:36 +0530

    Day 10 : Merge Sort [cont...]

--- Learning Algorithms with ChatGPT/6 Merge Sort/index.js
+++ Learning Algorithms with ChatGPT/6 Merge Sort/index.js
@@ -8,3 +8,27 @@ const mergeSort = (arr) => {
 
   return mergeSort(left, right);
 };
+
+const merge = (left, right) => {
+  const result = [];
+  while (!!left[0] && !!right[0]) {
+    if (left[0] <= right[0]) {
+      // console.log('pushing', left[0]);
+      result.push(left[0]);
+      left.shift();
+    } else {
+      //console.log("pushing", right[0]);
+      result.push(right[0]);
+      right.shift();
+    }
+
+    result = result.concat(left, right);
+  }
+
+  return result;
+};
+
+// example
+
+const unsortedArray = [12, 11, 13, 5, 6, 7];
+console.log(mergeSort(unsortedArray));
