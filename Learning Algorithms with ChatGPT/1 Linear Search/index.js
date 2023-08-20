d9e9021f6e81ed7ee9bd29660de31e6335f8e080
arunraj-ar <mail.arunrajar@gmail.com>
2023-08-20 23:53:39 +0530

    Day 13 : Linear Search [rev...]

--- Learning Algorithms with ChatGPT/1 Linear Search/index.js
+++ Learning Algorithms with ChatGPT/1 Linear Search/index.js
@@ -9,4 +9,22 @@ const linearSearch = (arr, target) => {
 
 const array = [4, 2, 8, 1, 6, 5];
 const target = 1;
-console.log(linearSearch(array, target)); 
+console.log(linearSearch(array, target));
+
+// revising
+
+const linearSearch2 = (arr, target) => {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+// example
+
+const arr = [4, 2, 8, 1, 6, 5, 0, -12];
+const tgt = 6;
+console.log(linearSearch2(arr, tgt));
