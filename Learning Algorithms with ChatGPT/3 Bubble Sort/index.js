c466795eebb39a706382c05e709dde1490f27599
arunraj-ar <mail.arunrajar@gmail.com>
2023-08-23 23:51:49 +0530

    Day 15 : Bubble Sort [rev...]

--- Learning Algorithms with ChatGPT/3 Bubble Sort/index.js
+++ Learning Algorithms with ChatGPT/3 Bubble Sort/index.js
@@ -17,3 +17,19 @@ const bubbleSort = (arr) => {
 
 const unsortedArray = [64, 34, 25, 12, 22, -100, 11, 90, 0];
 console.log(bubbleSort(unsortedArray));
+
+const bubbleSort2 = (arr) => {
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+};
+
+
+console.log(bubbleSort2(unsortedArray));
