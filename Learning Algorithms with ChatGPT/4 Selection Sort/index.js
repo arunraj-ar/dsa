9fdce3df4cb6bc8eedadc99d92f6c32994471591
arunraj-ar <mail.arunrajar@gmail.com>
2023-08-24 23:55:17 +0530

    Day 16 : Selection Sort [rev...]

--- Learning Algorithms with ChatGPT/4 Selection Sort/index.js
+++ Learning Algorithms with ChatGPT/4 Selection Sort/index.js
@@ -20,3 +20,28 @@ const selectionSort = (arr) => {
 
 const unsortedArray = [64, 25, 12, 22, 11, -5, 0];
 console.log(selectionSort(unsortedArray));
+
+// revising
+
+const selectionSort2 = (arr) => {
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    let minIdx = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+
+    [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+  }
+
+  return arr;
+};
+
+// example
+
+const unsortedArr = [64, 25, 12, 22, 11, -5, 0, 200, -200, 0, -5, 1, 12];
+console.log(selectionSort2(unsortedArr));
