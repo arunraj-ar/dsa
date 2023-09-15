c7f8af264ce76e9c1b6cbd57105e7ffc6b17a42c
arunraj-ar <mail.arunrajar@gmail.com>
2023-09-16 01:03:56 +0530

    Day 34 : Radix Sort [cont...]

--- Learning Algorithms with ChatGPT/10 Radix Sort/index.js
+++ Learning Algorithms with ChatGPT/10 Radix Sort/index.js
@@ -1,3 +1,14 @@
 const countingSort = (arr, exp) => {
-    const n = arr.length;
-}
+  const n = arr.length;
+};
+
+const radixSort = (arr) => {
+  const max = Math.max(...arr);
+
+  let exp = 1;
+
+  while (Math.floor(max / exp) > 0) {
+    countingSort(arr, exp);
+    exp *= 10;
+  }
+};
