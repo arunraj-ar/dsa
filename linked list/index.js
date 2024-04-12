b3932bb62345e668dbe65676a92de06964f034e4
Arun Raj <arunraj@CLLAP-CHN-1960.local>
2024-04-12 21:32:26 +0530

    linked list

---
+++ linked list/index.js
@@ -0,0 +1,26 @@
+// 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+
+let myLinkedList = {
+  head: {
+    value: 10,
+    next: {
+      value: 5,
+      next: {
+        value: 16,
+        next: {
+          value: 8,
+          next: {
+            value: 4,
+            next: {
+              value: 2,
+              next: {
+                value: 1,
+                next: null,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
