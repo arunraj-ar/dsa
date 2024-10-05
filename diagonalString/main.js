14dfc79d7f85a27bfe7db5367df8deb4baf67786
arunraj-ar <mail.arunrajar@gmail.com>
2024-10-06 00:00:55 +0530

    diagonalString

---
+++ diagonalString/main.js
@@ -0,0 +1,18 @@
+// Input: 
+// STR = FLIPKARTISHIRING
+// ROW = 4
+
+// Output: FRRLATIIIKIHNPSG
+
+// grid[x][y]
+const grid = Array.from({length: 4})
+let charIdx = 0
+while (charID>STR.length)
+    for (let i = 0; i<ROW ; i++) {
+        grid[0][i] = STR[charIdx]
+        charIdx++;
+    }
+    for(let i = 0 ; i<ROW-1;i++  {
+        grid[i-]
+    })
+    
